196598f68d7f618367f1424deff98d8c9d02013e
Punith Chandra Kandagatla <punithchandrak@gmail.com>
2026-06-17 23:44:33 +0530

    Fix Vercel build error

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Login.css";
 
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
